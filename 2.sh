3da1a32787f5c175a18b080dfa9d5e29fdd5fb20
MINERGITCOIN <132787165+MINERGITCOIN@users.noreply.github.com>
2025-01-25 17:14:26 +0300

    Create 2.sh

---
+++ 2.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+apt update && apt upgrade -f -y
+
+apt install unzip -y
+
+mkdir OrionMiner
+
+cd OrionMiner
+
+wget https://github.com/SL-x-TnT/OrionClient/releases/download/v.1.3.1.1/linux-x64-standalone.zip
+
+unzip linux-x64-standalone.zip
+
+cd linux-x64-standalone
+
+chmod +x OrionClient
+
+echo > settings.json
+
+cat  << EOF >> settings.json
+{
+  "Pool": "Excalivator Pool",
+  "GPUDevices": [
+    0
+  ],
+  "HasPrivateKey": false,
+  "KeyFile": "",
+  "PublicKey": "6GnZt5rXPZVKRMZjz8AWCPYD2NCpEgdkLy2k2H5xLNjY",
+  "MigratedSettings": false,
+  "CPUSetting": {
+    "CPUHasher": "Disabled",
+    "CPUThreads": 4,
+    "AutoSetCPUAffinity": true
+  },
+  "GPUSetting": {
+    "GPUHasher": "4090 Optimized",
+    "MaxGPUNoncePerBatch": 2048,
+    "GPUBlockSize": 512,
+    "ProgramGenerationThreads": 0,
+    "EnableExperimentalHashers": false
+  },
+  "VanitySetting": {
+    "GPUDevices": [],
+    "MaxRAMUsageMB": 1024,
+    "VanitySearchFile": "search.txt",
+    "VanityOutputFile": "foundWallets.txt",
+    "GPUBlockSize": 256,
+    "MinimumCharacterLength": 0,
+    "VanityThreads": 0
+  }
+}
+EOF
