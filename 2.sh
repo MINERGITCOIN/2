690353e141c6be8114551e7bd175cb4a5768fa64
MINERGITCOIN <132787165+MINERGITCOIN@users.noreply.github.com>
2025-01-25 18:42:16 +0300

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -14,7 +14,7 @@ cat  << EOF >> linux-x64-standalone/settings.json
 {
   "Pool": "Excalivator Pool",
   "GPUDevices": [
-    0 1 2 3 4 5 6 7 8 9 
+    0
   ],
   "HasPrivateKey": false,
   "KeyFile": "",
